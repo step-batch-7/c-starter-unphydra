d5586fa72d66799da293d2e2f0d0ccb372d741cb
rivu <58026024+unphydra@users.noreply.github.com>
2020-04-04 16:25:03 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-  
 #!/bin/bash
 
 filename=$1
 rm -rf bin
 mkdir bin
-file=`echo $filename | grep -o '/[a-zA-Z]*[a-z]'`
+file=`echo $filename |rev | cut -d "/" -f 1|rev| sed "s/.c$//"`
 
-gcc ./$filename -o bin$file && ./bin$file
+gcc ./$filename -o bin/$file && ./bin/$file
